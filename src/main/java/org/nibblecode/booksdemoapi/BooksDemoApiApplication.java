5831488378fffec80372b21ec360c2021328f899
adoug4729 <ozairprince07@gmail.com>
2024-02-19 12:36:55 +0500

    Project Setup

---
+++ src/main/java/org/nibblecode/booksdemoapi/BooksDemoApiApplication.java
@@ -0,0 +1,13 @@
+package org.nibblecode.booksdemoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksDemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksDemoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nibblecode/booksdemoapi/BooksDemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nibblecode.booksdemoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksDemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
